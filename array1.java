b8303275b8ae3f829b676ee42e2622a965d1bef1
saidarshan <46933205+darshan888@users.noreply.github.com>
2022-07-07 10:07:31 +0530

    commit

---
+++ array1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class array1{
+	public static void main(String args[]){
+		int arr[]= new int [5];
+		arr[0]=10;
+		arr[1]=12;
+		arr[2]=13;
+		arr[3]=24;
+		arr[4]=23;
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+}
---
+++ array3.java
@@ -0,0 +1,15 @@
+public class array3
+{
+    public static void main (String[] args)
+    {
+        int[] arr = new int[4];
+        arr[0] = 10;
+        arr[1] = 20;
+		arr[2]= 30;
+	    arr[3]= 40;
+		arr[4]= 50;
+  
+        for (int i = 0; i <= arr.length; i++)
+            System.out.println(arr[i]);
+    }
+}
